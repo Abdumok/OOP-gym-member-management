6f41927f924f24e36b879875efe81d680655b1cb
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-01-31 18:47:58 +0300

    add menu and new member functions

--- main.py
+++ main.py
@@ -17,4 +17,26 @@ class Membership:
         print(f"Status: {self.status}")
         print("----------------------------------")
 
+    # main menu
+    @staticmethod
+    def menu():
+        print("Welcome to GYM Membership Management\n")
+        print("1. Add new member")
+        print("2. Display all members")
+        print("3. Search for a member")
+        print("4. Exit")
+
+    @staticmethod
+    def new_member():
+        first_name = input("\nEnter your first name: ")
+        last_name = input("\nEnter your last name: ")
+        membership_id = input("\nEnter Membership id: ")
+        status = input("\nEnter membership status (active, inactive), or press enter: " )
+        if status == "active":
+            status = "active"
+        else:
+            status = "inactive"
+
+            return Membership(first_name, last_name, membership_id, status)
+
 
